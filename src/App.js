ca70194d8d14fa71b81ebcaa21566f73cf5559bd
“eenfeeneet” <burnhafiz@gmail.com>
2020-08-13 16:55:01 +0800

    First commit. Added bootstrap 5.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container vh-100 bg-secondary'>
+      <div className='card border-secondary mb-3'>
+        <div className='card-header'>Header</div>
+        <div className='card-body text-secondary'>
+          <h5 className='card-title'>Secondary card title</h5>
+          <p className='card-text'>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
